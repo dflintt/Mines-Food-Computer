8b205a664e4f41232e3e70af090cf3e64851f9a9
dflintt <dflint@mymail.mines.edu>
2017-03-19 17:07:51 -0600

    Added new class files (fridge and module)

---
+++ FoodComputerC/Module.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maker.RemoteWiring;
+using Microsoft.Maker.Serial;
+
+namespace FoodComputerC
+{
+    class Module
+    {
+        private RemoteDevice arduino; // If we have to use two unos, need to specify which one we'll be reading from
+        private int tempPin; // indicates pin of 
+        private string moisturePin; // Indicates pin of moisture sensor. Digital read fxn appears to want a string???
+        private int ledPin; // Test
+
+        public Module(RemoteDevice arduino, int tempPin, string moisturePin, int LEDPin) //constructor
+        {
+            this.arduino = arduino;
+            this.tempPin = tempPin;
+            this.moisturePin = moisturePin;
+            this.LEDPin = LEDPin;
+        }
+
+        public double readTemp() // Architecture of this and readHumidity depend on however firmata connection is implemented
+        {
+            return 0.0;
+        }
+
+        public double readHumidity()
+        {
+            return 0.0;
+        }
+
+        public int readMoisture()
+        {
+            return arduino.analogRead(moisturePin);
+        }
+    }
+}
